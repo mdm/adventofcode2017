506b0d3fdf3c9f098db51b477c0ed900e9fe2178
Marc Dominik Migge <marcmigge@gmx.net>
2022-01-13 13:38:27 +0100

    Finish day 1

---
+++ day01/day01.lisp
@@ -0,0 +1,28 @@
+(defun read-input (filename)
+    (with-open-file (stream filename)
+        (read-line stream nil)))
+
+(defun parse-input (input)
+    (map 'list #'(lambda (c) (digit-char-p c)) input))
+
+(defun rot-left(n l)
+    (append (nthcdr n l) (butlast l (- (length l) n))))
+
+(defun solve-captcha (input n)
+    (let ((rotated (rot-left n input)))
+        (reduce #'+
+            (map 'list #'(lambda (pair)
+                (if (= (first pair) (second pair)) (first pair) 0)) (mapcar #'list input rotated)))))
+
+(defun part1 (filename)
+    (solve-captcha (parse-input (read-input filename)) 1)) 
+
+(defun part2 (filename)
+    (let ((input (parse-input (read-input filename))))
+        (solve-captcha input (/ (length input) 2))))
+
+(defun day01 ()
+    (format t "~d~%" (part1 "input.txt"))
+    (format t "~d~%" (part2 "input.txt")))
+
+(day01)
